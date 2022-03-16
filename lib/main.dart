fc554aa59aa5b784ccf68d1b53a469c0bfa37fef
Chris Gabler <chris.gabler@uni-ulm.de>
2022-03-16 12:26:54 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyWidget(title: 'Titel', message: 'Nachricht'));
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({
+    Key? key,
+    required this.title,
+    required this.message,
+  }) : super(key: key);
+
+  final String title;
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text(message),
+        ),
+      ),
+    );
+  }
+}
