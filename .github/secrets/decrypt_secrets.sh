8981257c21a3a81f89663f832c7e97247d88be24
Chris Gabler <chris.gabler@uni-ulm.de>
2022-06-29 12:39:45 +0200

    test

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -9,8 +9,8 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./.github/secrets/Flutter_Pipeline_Demo.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/Flutter_Pipeline_Demo.mobileprovision.mobileprovision
 
 
-security create-keychain -p "password" build.keychain
-security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "password" -A
+security create-keychain -p "$IOS_KEYS" build.keychain
+security import ./.github/secrets/Certificate.p12 -t agg -k ~/Library/Keychains/build.keychain -P "$IOS_KEYS" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
