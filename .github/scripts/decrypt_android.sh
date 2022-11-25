71ce2c6ad6ba54898bd7fa95990837e373c1bf30
Chris Gabler <chris.gabler@uni-ulm.de>
2022-11-25 11:38:56 +0100

    test

--- .github/scripts/decrypt_android.sh
+++ .github/scripts/decrypt_android.sh
@@ -3,7 +3,8 @@
 # --yes to assume "yes" for questions
 
 gpg --quiet --batch --yes --decrypt --passphrase="$ANDROID_KEYS_ZIP_PASSPHRASE" \
---output android/key_files.zip android/key_files.zip.gpg && cd android && jar xvf key_files.zip && cd -ls -d $PWD/android/*
+--output android/key_files.zip android/key_files.zip.gpg && cd android && jar xvf key_files.zip && cd -
 
+ls -d $PWD/android/*
 mv ./android/flutterCICDkeystore.jks ./android/app
 # move your file according to path in key.properties
