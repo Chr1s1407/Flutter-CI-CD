6467323d0fc4246191ab7d308628a9ed968840ee
Chris Gabler <chris.gabler@uni-ulm.de>
2023-02-06 12:28:06 +0100

    Revert "updating images"
    This reverts commit 77ea85727c47765caae920b4f5151eaa869edb75.

--- lib/counter.dart
+++ lib/counter.dart
@@ -1,11 +1,7 @@
-import 'dart:io';
-
 class Counter {
   int value = 0;
 
   void increment() => value++;
 
   void decrement() => value--;
-
-  void doNothing() => sleep(const Duration(seconds: 2));
 }
